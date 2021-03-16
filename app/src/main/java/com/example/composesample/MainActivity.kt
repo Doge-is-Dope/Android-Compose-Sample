86d13ed8863b013676ba7d111c7899c5a05af3d3
doge-is-dope <maple3312936@outlook.com>
2021-03-16 23:10:58 +0800

    feat: add image compose

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composesample.ui.theme.ComposeSampleTheme
@@ -31,6 +35,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NewsStory() {
     Column(modifier = Modifier.padding(16.dp)) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(Modifier.height(16.dp))
+
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
