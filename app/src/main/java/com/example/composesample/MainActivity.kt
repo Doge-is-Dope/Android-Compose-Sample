df66b08a3a9226a105c39e68c21d3bf59b63cdba
doge-is-dope <maple3312936@outlook.com>
2021-03-16 23:02:53 +0800

    feat: add Jetpack Compose

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.composesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composesample.ui.theme.ComposeSampleTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            ComposeSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    NewsStory()
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun NewsStory() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    NewsStory()
 }
